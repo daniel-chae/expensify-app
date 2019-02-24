9d55e32c5fdd562b1072396e7b27a7135f667598
Hyun Chae <DanielChae@Hyuns-MacBook-Air.local>
2019-02-24 14:43:31 +0700

    Setup login functionality

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import Header from '../../components/Header';
-import toJSON from 'enzyme-to-json';
+import { Header } from '../../components/Header';
 import { shallow } from 'enzyme';
 
 //react-test-renderer
@@ -9,6 +8,13 @@ import { shallow } from 'enzyme';
 //What JSX comes back
 
 test('should render Header correctly', () => {
-    const wrapper = shallow(<Header />);
+    const wrapper = shallow(<Header startLogout={()=>{}} />);
     expect(wrapper).toMatchSnapshot();
 });
+
+test('should call startLogout when clicked', () => {
+    const startLogout = jest.fn();
+    const wrapper = shallow(<Header startLogout={startLogout} />);
+    wrapper.find('button').simulate('click');
+    expect(startLogout).toHaveBeenCalled()
+})
---
+++ src/tests/components/LoginPage.test.js
@@ -0,0 +1,15 @@
+import { shallow } from 'enzyme';
+import { LoginPage } from '../../components/LoginPage';
+import React from 'react';
+
+test('should render LoginPage', () => {
+    const wrapper = shallow(<LoginPage />);
+    expect(wrapper).toMatchSnapshot();
+})
+
+test('should call startLogin on button click', ()=>{
+    const startLogin = jest.fn();
+    const wrapper = shallow(<LoginPage startLogin={ startLogin } />);
+    wrapper.find('button').simulate('click');
+    expect(startLogin).toHaveBeenCalled()
+})
